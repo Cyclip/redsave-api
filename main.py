731bffb25b06f7008d36de940afb5734e450a4e5
Ifaz Ahmed <gdneop@gmail.com>
2022-07-15 17:33:26 +0100

    Handle conversion

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from sanic import Sanic
+from sanic import Sanic, response
 from dotenv import load_dotenv
+import pathlib
 import os
 import re
 import praw
 import funcs
 import socket
+import subprocess
+import hashlib
+import logging
+import asyncio
+import time
+
+MAX_FILE_STORED_TIME = 60 * 60 * 6 # 6 hours
 
 # bind to ipv6
 sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
@@ -24,7 +32,7 @@ reddit = praw.Reddit(
 )
 
 
-@app.route("/")
+@app.route("/getlink")
 async def get_link(req):
     url = req.args.get("url")
     
@@ -48,5 +56,58 @@ async def get_link(req):
 
     return funcs.generate_response(True, submission.url)
 
+
+@app.route("/getmp4")
+async def get_mp4(req):
+    # Example URL:
+    # https://v.redd.it/jeswo38hjtsif/HLSPlaylist.m3u8
+    url = req.args.get("url").encode()
+    filename = f'conversions/{hashlib.md5(url).hexdigest()}.mp4'
+
+    if not os.path.exists(filename):
+        # convert mu3u8 -> mp4 via ffmpeg.exe
+        logging.info(f"Converting {url} to {filename}")
+        try:
+            result = subprocess.run(
+                ['ffmpeg', '-i', url, '-c', 'copy', '-bsf:a', 'aac_adtstoasc', filename],
+                shell=True,
+                check=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.STDOUT,
+            )
+        except subprocess.CalledProcessError as e:
+            return funcs.generate_response(False, e.output.decode())
+        except Exception as e:
+            return funcs.generate_response(False, e)
+
+    
+    return await response.file(
+        filename,
+        mime_type='video/mp4',
+    )
+
+
+async def routine_delete_conversions():
+    while True:
+        try:
+            for file in os.listdir("conversions"):
+                path = os.path.join("conversions", file)
+
+                # get time since creation
+                time_since_creation = (time.time() - os.path.getctime(path))  # in seconds
+                
+                if path.endswith(".mp4") and time_since_creation >= MAX_FILE_STORED_TIME:
+                    os.remove(f"conversions/{path}")
+                    logging.info(f"Deleted {path}")
+        
+        except Exception as e:
+            logging.error(e)
+        
+        asyncio.sleep(60 * 60) # hourly
+
 if __name__ == "__main__":
+    pathlib.Path("./conversions").mkdir(parents=True, exist_ok=True)
+
+    app.add_task(routine_delete_conversions)
+
     app.run(sock=sock)
